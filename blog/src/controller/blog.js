924e8c4380ed8d3436fae4f99d4a97903d3b7cca
interesting1113 <sylviayang1113@gmail.com>
2022-01-04 21:42:04 +0900

    API对接mysql（博客列表）

--- blog/src/controller/blog.js
+++ blog/src/controller/blog.js
@@ -1,21 +1,16 @@
+const { exec} = require('../db/mysql')
 const getList = (author, keyword) => {
-  // 先返回假数据（格式是正确的）
-  return [
-    {
-      id: 1,
-      title: 'titleA',
-      content: 'contentA',
-      createTime: 1640347831602,
-      author: 'hyde'
-    },
-    {
-      id: 2,
-      title: 'titleB',
-      content: 'contentB',
-      createTime: 1640347880124,
-      author: 'hydeee'
-    }
-  ]
+  let sql = `select * from blogs where 1=1 `
+  if (author) {
+    sql += `and author='${author}' `
+  }
+  if (keyword) {
+    sql += `and title like '%${keyword}%' `
+  }
+  sql += `order by createtime desc;`
+  
+  // 返回 promise
+  return exec(sql)
 }
 
 const getDetail = (id) => {
