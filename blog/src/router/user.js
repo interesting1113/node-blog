a64c25b0cc2baf07854ebd318e6bf41667c94f5e
interesting1113 <sylviayang1113@gmail.com>
2022-01-11 21:03:03 +0900

    cookie做限制

--- blog/src/router/user.js
+++ blog/src/router/user.js
@@ -1,6 +1,13 @@
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  return d.toGMTString()
+}
+
 const handleUserRouter = (req, res) => {
   const method = req.method // GET POST
   const url = req.url
@@ -13,7 +20,7 @@ const handleUserRouter = (req, res) => {
     return result.then(data => {
       if (data.username) {
         // 操作 cookie
-        res.setHeader('Set-Cookie', `username=${data.username}; path=/`)
+        res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
         return new SuccessModel()
       }
       return new ErrorModel('登陆失败')
