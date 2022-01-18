d3e1f3face4ba22bdc45ccdca9d03b83f61acabc
interesting1113 <sylviayang1113@gmail.com>
2022-01-18 20:32:48 +0900

    express router

--- blog-express/app.js
+++ blog-express/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const blogRouter = require('./routes/blog');
+const userRouter = require('./routes/user');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/blog', blogRouter);
+app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
