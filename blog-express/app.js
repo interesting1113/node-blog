2cd8d3ed75ad8b6b4bc7f1e68ef7fb56dfbc4636
interesting1113 <sylviayang1113@gmail.com>
2022-01-20 21:40:00 +0900

    express redis

--- blog-express/app.js
+++ blog-express/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session)
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -23,13 +24,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+const redisClient = require('./db/redis')
+const sessionStore = new RedisStore({
+  client: redisClient
+})
 app.use(session({
   secret: 'WJiol#23123_',
   cookie: {
     // path: '/', // 默认配置
     // httpOnly: true, // 默认配置
     maxAge: 24 * 60 * 60 * 1000 
-  }
+  },
+  store: sessionStore
 }))
 
 // app.use('/', indexRouter);
--- blog-express/routes/user.js
+++ blog-express/routes/user.js
@@ -1,27 +1,53 @@
+const { login } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 var express = require('express')
 var router = express.Router()
 
 router.post('/login', function(req, res, next) {
   const { username, password } = req.body
-  res.json({
-    errno: 0,
-    data: {
-      username,
-      password
-    }
-  })
+    // const { username, password } = req.query
+    const result = login(username, password)
+    return result.then(data => {
+      if (data.username) {
+        // 设置 session
+        req.session.username = data.username
+        req.session.realname = data.realname
+      
+        res.json( 
+          new SuccessModel()
+        )
+        return
+      }
+      res.json( 
+        new ErrorModel('登陆失败')
+      )
+    })
 });
 
-router.get('session-test', (req, res, next) => {
-  const session = req.session
-  if (session.viewNum === 0) {
-    session.viewNum = 0
+router.get('/login-test', (req, res, next) => {
+  if (req.session.username) {
+    res.json({
+      errno: 0,
+      msg: '测试成功'
+    })
+    return
   }
-  session.viewNum ++
-
   res.json({
-    
+    errno: -1,
+    msg: '未登陆'
   })
 })
+
+// router.get('/session-test', (req, res, next) => {
+//   const session = req.session
+//   if (session.viewNum === 0) {
+//     session.viewNum = 0
+//   }
+//   session.viewNum ++
+
+//   res.json({
+
+//   })
+// })
 
 module.exports = router
